0c986867f53affc7ae03c4fd7bb9710f46a16862
Athar Mohammad <82324903+Athar996@users.noreply.github.com>
2023-02-04 11:32:03 +0530

    Api

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ const year =new Date();
 
 // getting all combine wednesday 14 july 2021 
 document.getElementById("daydate").innerHTML = days[weekday.getDay()] + " " + date.getDate() + "-" + months[month.getMonth()] + "-" + year.getFullYear();
+
+
+
+
+//weather api
+
+var button = document.querySelector('.button');
+var countrystate = document.querySelector('.countrystate');
+var temp = document.querySelector('.temp');
+var pressure = document.querySelector('.pressure');
+var windspeed = document.querySelector('.windspeed');
+
+
+button.addEventListener('click', function(){
+   
+    fetch('api.openweathermap.org/data/2.5/forecast/daily?q='+countrystate.value+'&appid=key')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    
+})
+
